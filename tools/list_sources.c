7cfd386217a47e2d0cbea3746a294b2944d6a86e
ianmartin <ianmartin@53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e>
2009-01-11 00:25:49 +0000

    Utilities added to tools directory
    list_sources creates a list of sources known to the eds. Could be
    useful when trying to connect to another source than the default.
    
    test_uri tries to open the passed uri as an EBook or ECal
    
    git-svn-id: http://svn.opensync.org/plugins/evolution2@5088 53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e

---
+++ tools/list_sources.c
@@ -0,0 +1,67 @@
+#include <glib.h>
+#include <libecal/e-cal.h>
+#include <libebook/e-book.h>
+#include <libedataserver/e-data-server-util.h>
+
+
+void print_sources(ESourceList *sources)
+{
+	ESource *source = NULL;
+
+	GSList *g = NULL;
+	for (g = e_source_list_peek_groups (sources); g; g = g->next) {
+		ESourceGroup *group = E_SOURCE_GROUP (g->data);
+		printf("Group: %s\n", e_source_group_peek_name(group));
+
+		GSList *s = NULL;
+		for (s = e_source_group_peek_sources (group); s; s = s->next) {
+			source = E_SOURCE (s->data);
+			printf("  %s: %s\n", e_source_peek_name(source), e_source_get_uri(source));
+		}
+	}
+}
+
+void print_calendars(ECalSourceType source_type) {
+	ESourceList *sources = NULL;
+	if (e_cal_get_sources(&sources, source_type, NULL)) {
+		print_sources(sources);
+	}
+}
+
+void print_memos()
+{
+	print_calendars(E_CAL_SOURCE_TYPE_JOURNAL);
+}
+
+void print_tasks()
+{
+	print_calendars(E_CAL_SOURCE_TYPE_TODO);
+}
+
+void print_events()
+{
+	print_calendars(E_CAL_SOURCE_TYPE_EVENT);
+}
+
+
+int main () {
+	ESourceList *sources = NULL;
+	printf("Addressbooks:\n");
+	if (e_book_get_addressbooks(&sources, NULL)) {
+		print_sources(sources);
+	}
+	printf("\n");
+	printf("Events:\n");
+	print_events();
+	printf("\n");
+
+	printf("Tasks:\n");
+	print_tasks();
+	printf("\n");
+	
+	printf("Memos:\n");
+	print_memos();
+
+
+	return 0;
+}
---
+++ tools/test_uri.c
@@ -0,0 +1,79 @@
+#include <string.h>
+#include <glib.h>
+#include <libecal/e-cal.h>
+#include <libebook/e-book.h>
+#include <libedataserver/e-data-server-util.h>
+
+void open_calendar(const char *uri, ECalSourceType source_type)
+{
+	ECal *cal = NULL;
+	GError *gerror = NULL;
+	cal = e_cal_new_from_uri(uri, source_type);
+
+	if (!cal) {
+		printf("Failed to allocate calendar\n");
+		return;
+	}
+	if (!e_cal_open(cal, TRUE, &gerror)) {
+		printf("Failed to open calendar:\n%s\n", gerror->message);
+		g_clear_error(&gerror);
+		return;
+	}
+	printf("Successfully opened %s\n", uri);
+	g_object_unref(cal);
+}
+
+void open_book(const char *uri)
+{
+	EBook *book = NULL;
+	GError *gerror = NULL;
+	book = e_book_new_from_uri(uri, &gerror);
+
+	if (book) {
+		if (e_book_open(book, TRUE, &gerror)) {
+			printf("Successfully opened %s\n", uri);
+		}
+		g_object_unref(book);
+	}
+	if (gerror) {
+		printf("Failed to open addressbook:\n%s\n", gerror->message);
+		g_clear_error(&gerror);
+	}
+}
+
+static void usage(char *progname, int exitcode)
+{
+	fprintf (stderr, "Usage: %s <objtype> <uri>\n\n", progname);
+	fprintf (stderr, "objtype may be one of:\n");
+	fprintf (stderr, "--contact\n");
+	fprintf (stderr, "--event\n");
+	fprintf (stderr, "--note\n");
+	fprintf (stderr, "--todo\n");
+	fprintf (stderr, "\nWARNING: This program allows you to attempt to open ECals with the wrong source type.  Doing so may break everything\n");
+	exit(exitcode);
+}
+
+int main(int argc, char *argv[])
+{
+
+	g_type_init();
+	if (argc < 2)
+		usage(argv[0], 1);
+	if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help"))
+		usage(argv[0], 0);
+		    
+	if (argc <3)
+		usage(argv[0], 1);
+	if (!strcmp(argv[1], "--contact")) {
+		open_book(argv[2]);
+	} else if (!strcmp(argv[1], "--event")) {
+		open_calendar(argv[2], E_CAL_SOURCE_TYPE_EVENT);
+	} else if (!strcmp(argv[1], "--todo")) {
+		open_calendar(argv[2], E_CAL_SOURCE_TYPE_TODO);
+	} else if (!strcmp(argv[1], "--note")) {
+		open_calendar(argv[2], E_CAL_SOURCE_TYPE_JOURNAL);
+	} else {
+		usage(argv[0], 1);
+	}
+	return 0;
+}
