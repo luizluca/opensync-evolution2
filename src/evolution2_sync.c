3a80cf5a8adfc8c93281ccf169065ecf0d436999
dgollub <dgollub@53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e>
2009-09-17 00:18:01 +0000

    Port evo2-sync and evo2 format plugin to format_env and plugin_env register API changes. See #1087
    git-svn-id: http://svn.opensync.org/plugins/evolution2@5776 53f5c7ee-bee3-0310-bbc5-ea0e15fffd5e

--- src/evolution2_sync.c
+++ src/evolution2_sync.c
@@ -221,7 +221,9 @@ osync_bool get_sync_info(OSyncPluginEnv *env, OSyncError **error)
 	osync_plugin_set_finalize(plugin, evo2_finalize);
 	osync_plugin_set_discover(plugin, evo2_discover);
 	
-	osync_plugin_env_register_plugin(env, plugin);
+	if (!osync_plugin_env_register_plugin(env, plugin, error))
+			goto error;
+
 	osync_plugin_unref(plugin);
 	
 	return TRUE;
